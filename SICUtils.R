ff3aa725adca6658ad5d0f12047b9ce776296ce2
mattmotyl <33463425+mattmotyl@users.noreply.github.com>
2018-09-13 13:30:11 -0500

    Update SICUtils.R
    Adding some more commentary to explain what lines are doing; added a line to replace responses containing a specific string to NA

--- SICUtils.R
+++ SICUtils.R
@@ -16,19 +16,23 @@
 
 #### Data Analysis ####
     
-    # Cleaning ANES data. You'll still need to make sure that they're coded in a way that makes sense after running this. 
-      # (ANES coding varies, so we can't automate making sure that the numbers make sense.)
+    # Cleaning data. You'll still need to make sure that they're coded in a way that makes sense after running this. 
+      # (Coding varies, especially across different publicly available sources, so we can't automate making sure that the numbers make sense.)
     
-    anesclean <- function(colsn){
-      # "x" is the column (or columns) of the variables that you'd like to clean.
-      colsn.r <- lapply(colsn, function(x) replace(x, grep("[-]", x), NA))
-      colsn.d <- as.data.frame(lapply(colsn.r, function(x) factor(x)))
-      colnum <- ncol(colsn)
-      colsn.d[,1:colnum] <- sapply(colsn.d[,1:colnum], as.numeric)
-      # Adding an indication that these are numeric. 
-      colnames(colsn.d) <- paste(colnames(colsn.d), "n", sep = ".")
-      colsn.d
-    }
+function(colsn){
+  # "x" is the column (or columns) of the variables that you'd like to clean.
+  # Problematic responses are coded as negative values; replace those as NA
+  colsn.r <- lapply(colsn, function(x) replace(x, grep("[-]", x), NA))
+  # Responses for respondents not yet eligible to complete certain questions are stored as "Not met"
+  # Replace those as NA
+  colsn.r <- lapply(colsn, function(x) replace(x, grep("Not met", x), NA))
+  colsn.d <- as.data.frame(lapply(colsn.r, function(x) factor(x)))
+  colnum <- ncol(colsn)
+  colsn.d[,1:colnum] <- sapply(colsn.d[,1:colnum], as.numeric)
+  # Adding an indication that these are numeric. 
+  colnames(colsn.d) <- paste(colnames(colsn.d), "n", sep = ".")
+  colsn.d
+}
 
 
 #### Plotting ####
